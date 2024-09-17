de8d21ad6a133d8bca5f7e2565668f1a36dd0898
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-09-17 10:50:35 +0600

    Component Composition: The special "children" Prop [Core Concept]

--- Section 3 - React Essentials - Components-JSX-Props-State_and_More/01-starting-project/src/App.jsx
+++ Section 3 - React Essentials - Components-JSX-Props-State_and_More/01-starting-project/src/App.jsx
@@ -1,6 +1,7 @@
 import { CORE_CONCEPTS } from './data.js';
 import CoreConcept from './components/CoreConcept.jsx';
 import Header from './components/Header/Header.jsx';
+import TabButton from './components/TabButton.jsx';
 
 
 
@@ -33,6 +34,15 @@ function App() {
               }
             />
           </ul>
+        </section>
+
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>
+              Components 
+            </TabButton>
+          </menu>
         </section>
       </main>
     </div>
