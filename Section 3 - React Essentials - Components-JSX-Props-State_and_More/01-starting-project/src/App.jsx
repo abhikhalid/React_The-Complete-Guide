6c6acf20d28c3b6566ee6ea5e070b8f19030cd50
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-09-17 10:26:32 +0600

    fix the path name

--- Section 3 - React Essentials - Components-JSX-Props-State_and_More/01-starting-project/src/App.jsx
+++ Section 3 - React Essentials - Components-JSX-Props-State_and_More/01-starting-project/src/App.jsx
@@ -3,6 +3,7 @@
 import componentsImg from './assets/components.png';
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
 
 
 
