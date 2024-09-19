9b079f810253bf79c1b172f91e459e8987e2a8fc
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-09-19 22:58:00 +0600

    User Input & Two-Way-Binding

--- Section 4 - React Essentials - Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ Section 4 - React Essentials - Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,24 +1,30 @@
 import React, { useState } from 'react';
 
-const Player = ({ name, symbol }) => {
+const Player = ({ initialName, symbol }) => {
+    const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
         setIsEditing((editing) => !editing);
     }
 
-    let playerName = <span className="player-name">{name}</span>;
+    function handleChange(event) {
+        console.log(event.target.value);
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
     // let btnCaption = 'Edit';
 
     if (isEditing) {
-        playerName = <input type="text" required value={name}/>;
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>;
         // btnCaption = 'Save';
     }
 
     return (
         <li>
             <span className="player">
-                {playerName}
+                {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
             {/* <button onClick={handleEditClick}>{btnCaption}</button> */}
