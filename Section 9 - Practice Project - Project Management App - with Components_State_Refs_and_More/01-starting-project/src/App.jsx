99ad3e2d6dc3839593095da9a07c88bb5f5985c9
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-09-22 10:41:29 +0600

    Adding the 'New Project' Component & A Reusable 'Input' Component

--- Section 9 - Practice Project - Project Management App - with Components_State_Refs_and_More/01-starting-project/src/App.jsx
+++ Section 9 - Practice Project - Project Management App - with Components_State_Refs_and_More/01-starting-project/src/App.jsx
@@ -1,9 +1,11 @@
+import NewProject from "./components/NewProject";
 import ProjectSidebar from "./components/ProjectSidebar";
 
 function App() {
   return (
-    <main className="h-screen my-8">
-      <ProjectSidebar/>
+    <main className="flex h-screen gap-8 my-8">
+      <ProjectSidebar />
+      <NewProject/>
     </main>
   );
 }
