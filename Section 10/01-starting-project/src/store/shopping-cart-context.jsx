1dcae01abcc358100e3ea4b8b05801cf564a143c
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-09-23 21:31:53 +0600

    Consuming the Context

--- Section 10/01-starting-project/src/store/shopping-cart-context.jsx
+++ Section 10/01-starting-project/src/store/shopping-cart-context.jsx
@@ -2,5 +2,5 @@ import { createContext } from "react";
 
 //The value that is actually produced by createContext will actually be an object that contains a React Component.
 export const CartContext = createContext({
-    items: []
+    items: [] //to get better auto-completion, we define here items (optional)
 });
