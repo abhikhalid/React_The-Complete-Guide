26d977e007f1afb21409fb8fe0bb70528c7a306a
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-09-23 21:00:00 +0600

    Creating & Providing The Context

---
+++ Section 10/01-starting-project/src/store/shopping-cart-context.jsx
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+//The value that is actually produced by createContext will actually be an object that contains a React Component.
+export const CartContext = createContext({
+    items: []
+});
