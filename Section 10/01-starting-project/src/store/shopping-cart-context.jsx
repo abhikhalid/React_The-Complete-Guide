dfbcde4577367a7bf122815a877aa5b741282a74
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-09-23 23:24:25 +0600

    Linking the Context to State

--- Section 10/01-starting-project/src/store/shopping-cart-context.jsx
+++ Section 10/01-starting-project/src/store/shopping-cart-context.jsx
@@ -2,5 +2,6 @@ import { createContext } from "react";
 
 //The value that is actually produced by createContext will actually be an object that contains a React Component.
 export const CartContext = createContext({
-    items: [] //to get better auto-completion, we define here items (optional)
+    items: [], //to get better auto-completion, we define here items (optional)
+    addItemToCart: () => {},
 });
