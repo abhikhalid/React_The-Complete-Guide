c4d70df6f4d43d75d852d2d4aabff402a34f8811
kmahmud <kmahmud@csafeglobal.com>
2024-09-28 13:49:23 +0600

    useEffect Not Needed: Another Example

--- Section 11/01-starting-project/src/App.jsx
+++ Section 11/01-starting-project/src/App.jsx
@@ -7,17 +7,34 @@ import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import {sortPlacesByDistance} from './loc.js';
 
+const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+const storedPlaces = storedIds.map(id => AVAILABLE_PLACES.find(place => place.id === id));
+
+
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
 
   const [availabePlaces, setAvailablePlaces] = useState([]);
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
 
-  //not all side effects require the usage of useEffect. because overusing useEffect and using it 
-  // unnecessarily is considered a bad practice. because you must not forget this is an extra execution cycle.
-  //avoid using useEffect if you don't need it.
+
+  //this is an example of reduntant usage od useEffect. 
+  //this code runs synchronously, so it doesn't need to be inside useEffect. It executes instantly and line by line.
+  //with local storage, we got not callback function or promises
+  //we can simply get rid of that useEffect.
   useEffect(() => {
+    // const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    // const storedPlaces = storedIds.map(id => AVAILABLE_PLACES.find(place => place.id === id));
+    
+    // setPickedPlaces(storedPlaces);
+  }, []);
+  
+
+   //but this was not the case here with getCurrentPosition. 
+  useEffect(() => {
+    //when this line executed here, it was not done yet. Instead it was only done once this callback function here was executed by the browser.
+    //and that happende at some point in the future.
     navigator.geolocation.getCurrentPosition((position)=>{
       const sortedPlaces = sortPlacesByDistance(
         AVAILABLE_PLACES,
@@ -53,7 +70,6 @@ function App() {
     if (storedIds.indexOf(id) === -1) {
       localStorage.setItem('selectedPlaces', JSON.stringify([id, ...storedIds]));
     }
-    
   }
 
   function handleRemovePlace() {
@@ -61,6 +77,14 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     modal.current.close();
+
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+
+    localStorage.setItem(
+      'selectedPlaces',
+      JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
+    )
+
   }
 
   return (
