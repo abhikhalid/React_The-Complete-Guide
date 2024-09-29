74695af61af6a652c4aad0f53ad647a3555a50c9
kmahmud <kmahmud@csafeglobal.com>
2024-09-29 14:58:45 +0600

    Section 12 END

--- Section 12 - Practice Project_Building Quiz App/01-starting-project/src/components/Quiz.jsx
+++ Section 12 - Practice Project_Building Quiz App/01-starting-project/src/components/Quiz.jsx
@@ -1,49 +1,31 @@
 import React, {  useCallback, useRef, useState } from 'react'
 import QUESTIONS from '../../questions';
-import quizIsCompleteImg from '../assets/quiz-complete.png';
 import QuestionTimer from './QuestionTimer';
 import Answers from './Answers';
 import Question from './Question';
+import Summary from './Summary';
 
 function Quiz() {
- const [answerState, setAnswerState] = useState('');
  const [userAnswers, setUserAnswers] = useState([]);
  
- const activeQuestionIndex = answerState === '' ? userAnswers.length : userAnswers.length - 1;
+ const activeQuestionIndex = userAnswers.length;
  
-
  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
- const handleSelectAnswer = useCallback( function handleSelectAnswer(selectedAnswer) {
-    setAnswerState('answered');
-
+ const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswers) => {
         return [...prevUserAnswers, selectedAnswer];
     });
-
-    setTimeout(() => {
-        if(selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]){
-           setAnswerState('correct');
-        }else{
-            setAnswerState('wrong');
-        }
-    
-      setTimeout(() => {
-        setAnswerState('');
-      }, 2000);
-    }, 1000);
-
- },[activeQuestionIndex]);
+ },[]);
 
 //  const handleSkipAnswer = useCallback(()=>handleSelectAnswer(null),[]);
  const handleSkipAnswer = useCallback(()=>handleSelectAnswer(null),[handleSelectAnswer]);
 
  if(quizIsComplete){
   return (
-    <div id='summary'>
-        <img src={quizIsCompleteImg} alt="Trophy Icon" />
-        <h2>Quiz Completed!</h2>
-    </div>
+   <Summary
+    userAnswers={userAnswers}
+   />
   )
  }
 
@@ -51,11 +33,8 @@ function Quiz() {
   return (
     <div id="quiz">
        <Question
-            key={activeQuestionIndex} //create new instance of QuestionTimer when activeQuestionIndex changes
-            questionText={QUESTIONS[activeQuestionIndex].text}
-            answers={QUESTIONS[activeQuestionIndex].answers}
-            answerState={answerState}
-            selectedAnswer={userAnswers[userAnswers.length - 1]}
+            key={activeQuestionIndex}
+            index={activeQuestionIndex}
             onSelectAnswer={handleSelectAnswer}
             onSkipAnswer={handleSkipAnswer} 
        />
