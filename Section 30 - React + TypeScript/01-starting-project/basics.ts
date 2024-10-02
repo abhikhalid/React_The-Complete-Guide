b1aae3218a207e80697f96731018b4006c5bdc90
kmahmud <kmahmud@csafeglobal.com>
2024-10-02 09:45:10 +0600

    Exploring the Base Types

---
+++ Section 30 - React + TypeScript/01-starting-project/basics.ts
@@ -0,0 +1,24 @@
+// Primitives: number, string, bollean
+// More complex types: arrays, objects, functions
+// Funtion types: parameters
+
+
+
+// Primitive types
+
+// let age : Number; //we want primitives not Number object
+let age : number;
+
+age = 12;
+
+let userName : string;
+
+userName = 'khalid';
+
+let isInstructor : boolean;
+
+isInstructor = true;
+
+let hobbies : null; // null is a type
+
+// hobbies = 12;
