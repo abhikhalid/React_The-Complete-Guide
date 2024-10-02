fa45081f7d954dd4f092e59188bbae6870791f94
kmahmud <kmahmud@csafeglobal.com>
2024-10-02 12:55:37 +0600

    Working with Props & TypeScript

--- Section 30 - React + TypeScript/my-app/src/components/Todos.tsx
+++ Section 30 - React + TypeScript/my-app/src/components/Todos.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 
-export default function Todos() {
+//There is also a special prop, the children prop; and we don't even to know the type of it.
+// Which we could find out which type that should be abd addded like this but it will quickly become cumbersome
+// it for every component  that  we're defining that is using props. we have to always add those built-in props  to that object and also
+// our custom props.
+
+// export default function Todos(props: {items: string[], children}) {
+
+
+const Todos: React.FC<{items: string[]}> =(props) => {
   return (
     <ul>
-        <li>Learn React</li>
-        <li>Learn TypeScript</li>
+        {
+           props.items.map((item) => (
+           <li key={item}>{item}</li>
+           ))}
     </ul>
   )
 }
+
+export default Todos;
