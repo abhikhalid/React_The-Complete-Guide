bc3ad345bb975b2565e41c07bf7adacd8cfd245c
kmahmud <kmahmud@csafeglobal.com>
2024-10-12 21:15:53 +0600

    Using Keys For Resetting Components

--- Section 13 - A Look Behind the Scenes of React and Optimization Techniques/01-starting-project/src/components/Counter/Counter.jsx
+++ Section 13 - A Look Behind the Scenes of React and Optimization Techniques/01-starting-project/src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import { useState, memo, useCallback, useMemo } from 'react';
+import { useState, memo, useCallback, useMemo, useEffect } from 'react';
 
 import IconButton from '../UI/IconButton.jsx';
 import MinusIcon from '../UI/Icons/MinusIcon.jsx';
@@ -38,6 +38,11 @@ const Counter = memo(function Counter({ initialCount }) {
     {value: initialCount, id: Math.random() * 1000}
     // {value: initialCount}
   ]);
+
+  // useEffect(() => {
+  //    setCounterChanges( [{value: initialCount, id: Math.random() * 1000}]);
+  // }, [initialCount]);
+  
 
   const currentCounter = counterChanges.reduce(
     (prevCounter, counterChange) => prevCounter + counterChange.value,
