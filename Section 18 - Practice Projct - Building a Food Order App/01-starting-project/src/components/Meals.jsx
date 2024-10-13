b4e5b4d5b11bc90b7d74faa4d244d86dae44cc3b
kmahmud <kmahmud@csafeglobal.com>
2024-10-13 23:30:23 +0600

    Adding a Custom HTTP Hook & Avoiding Common Errors

--- Section 18 - Practice Projct - Building a Food Order App/01-starting-project/src/components/Meals.jsx
+++ Section 18 - Practice Projct - Building a Food Order App/01-starting-project/src/components/Meals.jsx
@@ -1,25 +1,36 @@
 import React, { useEffect, useState } from 'react'
 import MealItem from './MealItem';
+import useHttp from '../hooks/useHttp';
+
+const requestConfig = {};
 
 //React Component can not be a async function
 export default function Meals() {
-  const [loadedMeals, setLoadedMeals] = useState([]);
+  // const [loadedMeals, setLoadedMeals] = useState([]);
   
   //firt write code without useEffect to show your students about infinite loop
-  useEffect(() => {
-    async function fetchMeals(){
-      const response = await fetch('http://localhost:3000/meals');
+  // useEffect(() => {
+  //   async function fetchMeals(){
+  //     const response = await fetch('http://localhost:3000/meals');
   
-      if(!response.ok){
-        // ...
-      }
+  //     if(!response.ok){
+  //       // ...
+  //     }
   
-      const meals = await response.json();
-      setLoadedMeals(meals);
-    }
+  //     const meals = await response.json();
+  //     setLoadedMeals(meals);
+  //   }
 
-    fetchMeals();
-  }, []);
+  //   fetchMeals();
+  // }, []);
+
+  const {data : loadedMeals, isLoading, error} = useHttp('http://localhost:3000/meals',requestConfig,[]);
+
+  console.log(loadedMeals);
+
+  if(isLoading){
+    return <p>Fetching meals...</p>;
+  }
   
   
   return (
