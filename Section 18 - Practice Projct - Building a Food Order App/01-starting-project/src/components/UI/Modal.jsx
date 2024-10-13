c105a59a864a8ad15ecf55a8a141b9ebbc1032f8
kmahmud <kmahmud@csafeglobal.com>
2024-10-13 20:49:19 +0600

    Adding a Custom Input Component & Managing Modal Visibility

--- Section 18 - Practice Projct - Building a Food Order App/01-starting-project/src/components/UI/Modal.jsx
+++ Section 18 - Practice Projct - Building a Food Order App/01-starting-project/src/components/UI/Modal.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from 'react'
 import { createPortal } from 'react-dom'
 
-export default function Modal({children, open, className = ''}) {
+export default function Modal({children, open, onClose, className = ''}) {
   const dialog = useRef(); 
 
   useEffect(() => {
@@ -17,8 +17,10 @@ export default function Modal({children, open, className = ''}) {
 
   // I want to output it with portal feature React offers  so that we can use this Modal component from anywhere in our component tree
   // But we'll always inject the dialogue when it's visible in a specific area of the real DOM
+
+  // onCLose will trigger if use prsses on ESC key or clicks outside the modal
   return createPortal(
-    <dialog ref={dialog} className={`modal ${className}`}>
+    <dialog ref={dialog} className={`modal ${className}`} onClose={onClose}>
         {children}
     </dialog>,
   document.getElementById('modal'));
