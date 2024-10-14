8246d0e9669e919d49e9ea5ca98185db7938f3ff
kmahmud <kmahmud@csafeglobal.com>
2024-10-14 19:55:58 +0600

    Enabled & Disabled Queries

--- Section 24 -  React Query - Transtack Query - Handling HTTP Requests wtih Ease/starting-project/src/components/Events/FindEventSection.jsx
+++ Section 24 -  React Query - Transtack Query - Handling HTTP Requests wtih Ease/starting-project/src/components/Events/FindEventSection.jsx
@@ -7,11 +7,12 @@ import EventItem from './EventItem';
 
 export default function FindEventSection() {
   const searchElement = useRef();
-  const [searchTerm, setSearchTerm] = useState('');
-
- const {data, isPending, isError, error}  = useQuery({
+  const [searchTerm, setSearchTerm] = useState();
+// the difference between isLoading and isPending is that loading will not be true if this query is just disabled.
+ const {data, isLoading, isError, error}  = useQuery({
     queryKey: ['events', {search: searchTerm}],
     queryFn: ({signal}) => fetchEvents({signal,searchTerm}),
+    enabled: searchTerm !== undefined
   });
 
   function handleSubmit(event) {
@@ -21,7 +22,7 @@ export default function FindEventSection() {
 
   let content = <p>Please enter a search term and to find events.</p>;
 
-  if(isPending){
+  if(isLoading){
     content = <LoadingIndicator/>
   }
 
