8d3b54547a615d068d0a4531d6572ad5c706636d
kmahmud <kmahmud@csafeglobal.com>
2024-10-14 21:16:22 +0600

    Changing Data with Mutations

--- Section 24 -  React Query - Transtack Query - Handling HTTP Requests wtih Ease/starting-project/src/components/Events/NewEvent.jsx
+++ Section 24 -  React Query - Transtack Query - Handling HTTP Requests wtih Ease/starting-project/src/components/Events/NewEvent.jsx
@@ -2,24 +2,39 @@ import { Link, useNavigate } from 'react-router-dom';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import { useMutation } from '@tanstack/react-query';
+import { createNewEvent } from '../../util/http.js';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function NewEvent() {
   const navigate = useNavigate();
 
-  function handleSubmit(formData) {}
+  const {mutate, isPending, isError, error} = useMutation({
+    // mutationKey:  we could set mutationKey here, but we don't need it here as mutations typically isn't to cache their response data
+    // they are primarliy about changing something on your backend. not about getting and storing data in your front-end.
+    mutationFn: createNewEvent
+  });
+
+  function handleSubmit(formData) {
+    mutate({
+      event: formData,
+    });
+  }
 
   return (
     <Modal onClose={() => navigate('../')}>
       <EventForm onSubmit={handleSubmit}>
-        <>
+        {isPending && 'Submitting...'}  
+        {!isPending && (<>
           <Link to="../" className="button-text">
             Cancel
           </Link>
           <button type="submit" className="button">
             Create
           </button>
-        </>
+        </>)}
       </EventForm>
+      {isError && <ErrorBlock title="Failed to create event" message={error.info?.message || 'Failed to create event. Please check your inputs and try agian later.'} />}
     </Modal>
   );
 }
