74f252755bf3a73adffb70cc4f20db7876825500
kmahmud <kmahmud@csafeglobal.com>
2024-10-14 21:35:21 +0600

    Fetching More Data & Testing the Mutation

--- Section 24 -  React Query - Transtack Query - Handling HTTP Requests wtih Ease/starting-project/src/util/http.js
+++ Section 24 -  React Query - Transtack Query - Handling HTTP Requests wtih Ease/starting-project/src/util/http.js
@@ -22,7 +22,7 @@ export  async function fetchEvents({signal, searchTerm}) {
     }
 
 
-    export async function createNewEvent(eventData) {
+export async function createNewEvent(eventData) {
         const response = await fetch(`http://localhost:3000/events`, {
           method: 'POST',
           body: JSON.stringify(eventData),
@@ -41,4 +41,20 @@ export  async function fetchEvents({signal, searchTerm}) {
         const { event } = await response.json();
       
         return event;
-   }
+}
+
+
+export async function fetchSelectableImages({ signal }) {
+    const response = await fetch(`http://localhost:3000/events/images`, { signal });
+  
+    if (!response.ok) {
+      const error = new Error('An error occurred while fetching the images');
+      error.code = response.status;
+      error.info = await response.json();
+      throw error;
+    }
+  
+    const { images } = await response.json();
+  
+    return images;
+}
